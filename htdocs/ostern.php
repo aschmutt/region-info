97050c9238798e00f2ab298b4499a20dcf4ab2b1
aschmutt <andrea@schmutt.de>
2020-04-09 20:38:37 +0200

    neue Inhalte

--- htdocs/ostern.php
+++ htdocs/ostern.php
@@ -10,5 +10,6 @@ $twig = new \Twig\Environment($loader, [
 echo $twig->render('ostern.twig', [
     'title' => 'Ostern',
     'description' => 'Ostern in Gessertshausen und Umgebung',
-    'url' => 'https://www.gessertshausen-info.de/ostern.php'
+    'url' => 'https://www.gessertshausen-info.de/ostern.php',
+    'image' => 'https://www.gessertshausen-info.de/Img/osterkorb_640.jpg'
 ]);
